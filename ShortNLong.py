48d2cb568b0a045211e80fb3ce57d2713a59a5f5
JensRosaman <oliver.granang@elev.ga.ntig.se>
2023-09-24 01:35:34 +0200

    started gameplayloop

--- ShortNLong.py
+++ ShortNLong.py
@@ -1,6 +1,8 @@
 import random
 import copy
 import itertools
+from flask import Flask
+
 
 class Card:
     """Represents a single card"""
@@ -115,7 +117,7 @@ class Card:
 
 
 class Deck:
-    """Represents both the deck on the table, has the hidden deck in self.deck and playedcarddeck for mid deck"""
+    """Represents the deck on the table  """
     def __init__(self) -> None:
 
         # creates the deck where each item is a card object
@@ -123,8 +125,10 @@ class Deck:
         # creates played card deck and plays a starting card
         self.init_new_round()
 
+    # old unefficant method 
+    """
     def _create_deck_(self) -> None:
-        """Creates a deck and shuffles"""
+        '''Creates a deck and shuffles'''
         self.deck = []
         suits = 'HCDS'
         ranks = 'A23456789TJQK'
@@ -133,31 +137,47 @@ class Deck:
                 for rank in ranks:
                     code = suit + rank
                     self.deck.append(Card(code))
+    """
 
-    def remove_card(self, i:list, deck: list) -> None:
-        """Removes a card from the deck and returns the item"""
+    # more efficant version using itertools
+    def _create_deck_(self) -> None:
+        """Creates a deck and shuffles"""
+        suits = 'HCDS'
+        ranks = 'A23456789TJQK'
+        # Use itertools.product to generate all card combinations
+        all_cards = [''.join(card) for card in itertools.product(suits, ranks)]
+        
+        # Duplicate the cards and store them in the deck
+        self.deck = [Card(code) for code in all_cards * 2]
+        
+        # Shuffle the deck
+        random.shuffle(self.deck)
+
+    def remove_card(self, cardsToRemove:list) -> None:
+        """
+        Removes a card from the deck and returns the item
+        deck - what deck to pop from
+        i - list of items to remove
+        
+        """
         popped = []
-        for item in i:
-            popped.append(deck.pop(item))
+        for item in cardsToRemove:
+            popped.append(self.deck.pop(item))
         return popped
 
-    def get_top_cards(self, i):
-        """gets the  top cards in the played cards deck"""
-        return self.playedCardDeck[i:]
-
+    
     def hand_out_cards(self,playerAmount,cardAmount) -> list:
         """Removes cards from the deck and returns a list of lists with each players hand"""
         playerHands = []
         for player in playerAmount:
-            cardsToGive = self.remove_card(self.deck,[cardAmount])
+            cardsToGive = self.remove_card(self.deck[:cardAmount])
             playerHands.append(cardsToGive)
         return playerHands
 
     def init_new_round(self):
+        """Creates a new round by res"""
         random.shuffle(self.deck)
         # removes the top cards and assigns it to the played deck
-        self.playedCardDeck = [(self.remove_card([-1], self.deck))[0]]
-        self.layingCard = self.playedCardDeck[-1]
 
 # _______________________________________________________________________________________________
 
@@ -170,9 +190,10 @@ class Player:
         self.complete_hand = False
         self.round = 1
         self.set_count = 0
-        self.run_count = 0
+        self.run_count = 0 
+        self.turn = False
 
-    # -------------------------------------Win conditions ----------------------------------------------------------
+    # ------------------------------------- Win conditions ----------------------------------------------------------
 
     def __3_of_a_kind__(self) -> None:
         """Gives the amount of 3 of a kinds in the instances hand"""
@@ -201,8 +222,6 @@ class Player:
         for rank in ranks:
             if all(rank in unique_ranks for rank in ranks[ranks.index(rank):ranks.index(rank) + 4]):
                 self.run_count += 1
-
-    # --------------------------------------------------------------------------------------
 
     def __complete_hand__(self):
         self.__3_of_a_kind__()
@@ -234,22 +253,118 @@ class Player:
         self.hand += cards_to_add
         self.__complete_hand__()
 
+    def get_score(self):
+        """Adds upp the total score of the players hand"""
+        score = 0
+        for card in self.hand:
+            score += card._point_value
+
 
 class Game:
     """Handles all the internal logic of the game"""
     def __init__(self, playerIDS: list) -> None:
+        self.numOfPlayers = None
+        self.playerIDs = []
+        self.deck = Deck()
+        self.players = {}
+        self._playOrder = []
+        self.discardDeck = []
+        self.round = 0
+        self.currentPlayer = None
+
+    def start_game(self, playerIDS) -> list:
+        """Starts the gameplay loop"""
         self.numOfPlayers = len(playerIDS)
         self.playerIDs = playerIDS
         self.deck = Deck()
-        self.players
-    def _hand_out_cards(self):
-        """Hands out cards to players"""
-        cardsToGive = self.deck.hand_out_cards(playerAmount=self.numOfPlayers, cardAmount=3)
-        for hand in cardsToGive:
-            pass
         
-        
+        # assing value to players list and start first round
+        self._hand_out_cards(6)
+        self._playOrder = list(self.players)
     
+        # laying out starting cards
+        self.discardDeck.append(self.deck.remove_card(self.deck.deck[-1]))
+        print(f"Game started, current laying card is {self.deck.layingCard}. Gameplay order is {self._playOrder} \n {self.players}")
+
+        # start of gameplay loop
+        gameLoop = True
+        while gameLoop:
+            
+            # Loops through the players, two for loops so each player takes turns starting
+            for i in self._playOrder:
+                self.round += 1 # next turn starting
+                for i in self._playOrder: # i is the player id of the current player
+                    self.currentPlayer = self.players[i] # indexs players after the id - gives the player object
+
+
+                    # -------------------checks if anyone wants to pick from discard
+
+
+                    # start of the turn of the current player - starts when picking up a card
+                    self.get_current_state(i) # updates the current state for the current player
+                    # cases that define each diffrent 
+
+    def _hand_out_cards(self, cardAmount):
+        """Hands out cards to players and creates a playerlist, reset deck!!!"""
+        cardsToGive = self.deck.hand_out_cards(playerAmount=self.numOfPlayers, cardAmount=cardAmount)
+        # creating index to iterate through the players
+        i = 0
+        for hand in cardsToGive:
+            self.players[self.playerIDs[i]] = Player(player_id=self.playerIDs[i], cards=hand)
+            i += 1
+            
+    def _calculate_penalty(self, positionInStack:int, player:Player):
+        """Takes the position of the stack and returns the amount the player has to take"""
+        negIndex = positionInStack - len(self.deck.deck)
+        cardsOnTop = self.deck.deck[negIndex:]
+        penaltyCards = len(cardsOnTop)
+
+        # if its the players turn avoid extra cards
+        if player.turn:
+           return penaltyCards - 1
+        else:
+            return penaltyCards
+    
+    def current_win_conditions(self):
+        """Sets the current win conditions depending on the round"""
+        winConditions = {"sets": None, "runs": None}
+        if self.round == 1:
+            winConditions['sets'] = 2
+                
+        elif self.round == 2:
+            winConditions["sets"] = 1 
+            winConditions['runs'] = 1
+
+        elif self.round == 3:
+            winConditions["runs"]= 2
+
+        elif self.round == 4:
+            winConditions["sets"] = 3
+        else:
+            pass
+        return winConditions
+    def get_current_state(self, playerId:int):
+        """Collects all the current game information avalible to the player"""
+        requestingPlayer = self.players[playerId] # indexes the players dict for the instance of the requested player
+        self.state = {
+            "discard": self.discardDeck,
+            "round": self.round,
+            "winConditions": self.current_win_conditions(),
+            "playOrder": self._playOrder,
+            "currentPlayer": self.currentPlayer,
+            "isCurrentPlayer": self.currentPlayer == requestingPlayer,
+            "hand": requestingPlayer.hand,
+            "winner": None,
+            "currentScore": requestingPlayer.get_score(),
+        }
+        return self.state
+    
+    
+    
+class humanAgent:
+    def __init__(self) -> None:
+        self.human = True
+        pass
 
 
 
@@ -265,7 +380,8 @@ def FY_Shuffle(items: list):
             items[i] = items[j]
             items[j] = tmp
         return items
-    
+
+# playerId är objekt för att repsentera den som ger instruktioner till spel modulen
 
 if __name__ == "__main__":
     spelare = Player([1],)
--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-import random
+from flask import Flask, jsonify, request , url_for
+import asyncio
+import threading
 
-numbers = []
-for x in range(1, 11):
-    numbers.append(x)
+# Handles several connections from diffrent endpoints and connects them to the main script
 
-for i in range(9):
-    random.shuffle(numbers)
-    print(numbers)
+def Main():
+    app = Flask(__name__)
+
+    @app.route("/t")
+    def t():
+        print("hej")
+
+    with app.app_context():
+        links = {"t": url_for("t")}
+        print(links)
+    
+
+
+if __name__ == "__main__":
+    p = {"d":{"pp":"gg"}}
+    print(p)
+
